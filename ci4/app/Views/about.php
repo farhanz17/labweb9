faa5c08eae612eeb451c52cf1b88afb936dd2f51
farhanz17 <mufhan623@gmail.com>
2023-06-06 11:53:04 +0700

    ditambahkan

--- ci4/app/Views/about.php
+++ ci4/app/Views/about.php
@@ -8,9 +8,9 @@
     <?= $this->include('template/header'); ?>
     <section id="about">
         <div class="row">
-            <img src="herli.JPG" title="Herliyansyah" alt="Herliyansyah" width="200" style="float: left; border: 1px solid black;">
+            <img src="farhan.jpg" title="Herliyansyah" alt="Herliyansyah" width="200" style="float: left; border: 1px solid black;">
             <h1>HI!</h1>
-            <p>Saya adalah Herliyansyah dari Universitas Pelita Bangsa dengan jurusan teknik informatika,hal yang saya ingin kuasai adalah <b>Frontend Developer</b> dalam membuat web. See uuu :-) </p>
+            <p>Saya adalah Muhammad Farhan dari Universitas Pelita Bangsa dengan jurusan teknik informatika,hal yang saya ingin kuasai adalah <b>Frontend Developer</b> dalam membuat web. See uuu :-) </p>
         </div>
     </section>
     <?= $this->include('template/footer'); ?>
